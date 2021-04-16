7335280670669436449357a3425e0a7c03e09d5d
Pedro Guerra <pguerra@cambridgeassociates.com>
2021-04-16 11:00:09 -0300

    updated port number

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ if(process.env.NODE_ENV === 'production') {
     })
 }
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
