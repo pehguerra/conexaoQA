6cfaa3d2b975e21862ee9b3727f2af932ebf782b
Pedro Guerra <pguerra@cambridgeassociates.com>
2021-04-16 00:45:12 -0300

    updated index.js background to deploy to prod

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 
 const app = express()
 
@@ -9,13 +10,21 @@ connectDB()
 // init middleware to work with json (body and response)
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API Running'))
-
 // define routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+// serve statics assets in production
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.port || 5000
 
